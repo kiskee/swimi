138522859ba0bd7f068d1fbce8fc3547314f71dd
Daniel Medina Cossio <daniel.medina@talent.com>
2023-10-10 16:39:53 -0500

    adding the login and profile page

--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -1,52 +1,125 @@
-import { withAuthenticator } from "@aws-amplify/ui-react";
-import { Auth } from "aws-amplify";
+import { Authenticator } from "@aws-amplify/ui-react";
+import { Auth, Hub } from "aws-amplify";
 import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
+import svgLogo from "../../../public/final.png";
+import Image from "next/image";
+import "@aws-amplify/ui-react/styles.css";
+import { Link } from "@aws-amplify/ui-react";
+
+const components = {
+  Header() {
+    return (
+      <div className="items-center mb-4 ">
+        <Image alt="svgnatacion-logo" src={svgLogo} height={200} width={200} />
+      </div>
+    );
+  },
+};
 
 function Profile() {
+  const [signedInUser, setSignedInUser] = useState(false);
   const [user, setUser] = useState(null);
   const router = useRouter();
   useEffect(() => {
-    checkUser();
-  }, []);
+    authListener();
+  }, [signedInUser]);
 
-  async function checkUser() {
-    const user = await Auth.currentAuthenticatedUser();
-    setUser(user);
+  async function authListener() {
+    Hub.listen("auth", (data) => {
+      switch (data.payload.event) {
+        case "signIn":
+          return setSignedInUser(true);
+        case "signOut":
+          setSignedInUser(false);
+          setAdmin("");
+          break;
+      }
+    });
+    try {
+      const user = await Auth.currentAuthenticatedUser();
+      setSignedInUser(true);
+      setUser(user);
+    } catch (err) {}
   }
-  //console.log(user);
 
   async function logOut() {
     try {
       const outUser = await Auth.signOut();
-      router.push(`/`);
+      //router.push(`/`);
     } catch (error) {}
   }
 
-  if (!user) return <>Not</>;
+  if (!user)
+    return (
+      <Authenticator
+        signUpAttributes={["birthdate", "email", "gender", "name"]}
+        className="mt-12"
+        components={components}
+      />
+    );
   return (
-    <>
-      <div>
-        <h1
-          className="text-3xl font-semibold tracking-wide
-            mt-6"
-        >
-          Profile
+    <Authenticator
+      signUpAttributes={["birthdate", "email", "gender", "name"]}
+      className="mt-12"
+      components={components}
+    >
+      <>
+        <h1 className="text-center text-indigo-950 text-4xl font-bold my-4">
+          Perfil
         </h1>
-        <h1 className="font-medium text-gray-500 my-2">
-          Username: {user.attributes.name}
+        <div className="grid grid-flow-col justify-stretch">
+          <Link
+            class="inline-block rounded bg-success px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#14a44d] transition duration-150 ease-in-out hover:bg-success-600 hover:shadow-[0_8px_9px_-4px_rgba(20,164,77,0.3),0_4px_18px_0_rgba(20,164,77,0.2)] focus:bg-success-600 focus:shadow-[0_8px_9px_-4px_rgba(20,164,77,0.3),0_4px_18px_0_rgba(20,164,77,0.2)] focus:outline-none focus:ring-0 active:bg-success-700 active:shadow-[0_8px_9px_-4px_rgba(20,164,77,0.3),0_4px_18px_0_rgba(20,164,77,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(20,164,77,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(20,164,77,0.2),0_4px_18px_0_rgba(20,164,77,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(20,164,77,0.2),0_4px_18px_0_rgba(20,164,77,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(20,164,77,0.2),0_4px_18px_0_rgba(20,164,77,0.1)] ml-4 text-center"
+            href="/"
+          >
+            Inicio
+          </Link>
+          <Link
+            class="inline-block rounded bg-danger px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#dc4c64] transition duration-150 ease-in-out hover:bg-danger-600 hover:shadow-[0_8px_9px_-4px_rgba(220,76,100,0.3),0_4px_18px_0_rgba(220,76,100,0.2)] focus:bg-danger-600 focus:shadow-[0_8px_9px_-4px_rgba(220,76,100,0.3),0_4px_18px_0_rgba(220,76,100,0.2)] focus:outline-none focus:ring-0 active:bg-danger-700 active:shadow-[0_8px_9px_-4px_rgba(220,76,100,0.3),0_4px_18px_0_rgba(220,76,100,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(220,76,100,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(220,76,100,0.2),0_4px_18px_0_rgba(220,76,100,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(220,76,100,0.2),0_4px_18px_0_rgba(220,76,100,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(220,76,100,0.2),0_4px_18px_0_rgba(220,76,100,0.1)] ml-4 text-center"
+            href="/blog"
+          >
+            Blog
+          </Link>
+          <Link
+            href="/about"
+            class="inline-block rounded bg-warning px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#e4a11b] transition duration-150 ease-in-out hover:bg-warning-600 hover:shadow-[0_8px_9px_-4px_rgba(228,161,27,0.3),0_4px_18px_0_rgba(228,161,27,0.2)] focus:bg-warning-600 focus:shadow-[0_8px_9px_-4px_rgba(228,161,27,0.3),0_4px_18px_0_rgba(228,161,27,0.2)] focus:outline-none focus:ring-0 active:bg-warning-700 active:shadow-[0_8px_9px_-4px_rgba(228,161,27,0.3),0_4px_18px_0_rgba(228,161,27,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(228,161,27,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(228,161,27,0.2),0_4px_18px_0_rgba(228,161,27,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(228,161,27,0.2),0_4px_18px_0_rgba(228,161,27,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(228,161,27,0.2),0_4px_18px_0_rgba(228,161,27,0.1)] ml-4 text-center"
+          >
+            Acerca
+          </Link>
+          <Link
+            href="/contact"
+            class="inline-block rounded bg-info px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#54b4d3] transition duration-150 ease-in-out hover:bg-info-600 hover:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.3),0_4px_18px_0_rgba(84,180,211,0.2)] focus:bg-info-600 focus:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.3),0_4px_18px_0_rgba(84,180,211,0.2)] focus:outline-none focus:ring-0 active:bg-info-700 active:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.3),0_4px_18px_0_rgba(84,180,211,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(84,180,211,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.2),0_4px_18px_0_rgba(84,180,211,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.2),0_4px_18px_0_rgba(84,180,211,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.2),0_4px_18px_0_rgba(84,180,211,0.1)] ml-4 text-center"
+          >
+            Contacto
+          </Link>
+        </div>
+        
+        <div className="flex justify-center mt-12">
+        <div class="block max-w-[18rem] rounded-lg bg-indigo-950 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)]">
+          <div class="border-b-2 border-[#0000002d] px-6 py-3 text-white dark:text-neutral-50">
+          <h1 className="text-white text-center text-bold text-xl ">
+          Informacion Personal:
         </h1>
-        <p className="text-sm text-gray-500 mb-6">
-          Email: {user.attributes.email}
-        </p>
-        <button
-          className="block py-2 pl-3 pr-4 rounded hover:bg-gray-100  md:p-0 text-red-600"
-          onClick={logOut}
-        >
-          Signout
-        </button>
-      </div>
-    </>
+          </div>
+          <div class="p-6">
+          <h1 className="font-medium text-white my-2">
+            Username: {user.attributes.name}
+          </h1>
+          <p className="text-sm text-white mb-6">
+            Email: {user.attributes.email}
+          </p>
+          <button
+            className="inline-block rounded bg-danger px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#dc4c64] transition duration-150 ease-in-out hover:bg-danger-600 hover:shadow-[0_8px_9px_-4px_rgba(220,76,100,0.3),0_4px_18px_0_rgba(220,76,100,0.2)] focus:bg-danger-600 focus:shadow-[0_8px_9px_-4px_rgba(220,76,100,0.3),0_4px_18px_0_rgba(220,76,100,0.2)] focus:outline-none focus:ring-0 active:bg-danger-700 active:shadow-[0_8px_9px_-4px_rgba(220,76,100,0.3),0_4px_18px_0_rgba(220,76,100,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(220,76,100,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(220,76,100,0.2),0_4px_18px_0_rgba(220,76,100,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(220,76,100,0.2),0_4px_18px_0_rgba(220,76,100,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(220,76,100,0.2),0_4px_18px_0_rgba(220,76,100,0.1)] ml-4 text-center"
+            onClick={logOut}
+          >
+            Cerrar Session
+          </button>
+          </div>
+        </div>
+        </div>
+      </>
+    </Authenticator>
   );
 }
-export default withAuthenticator(Profile);
+export default Profile;
