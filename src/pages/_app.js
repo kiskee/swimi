e95e8be21f22ce1a1646ff460f19dee2aa1af19d
Daniel Medina Cossio <daniel.medina@talent.com>
2023-10-11 11:59:32 -0500

    adding meta description for the site

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,11 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
 import { Amplify } from "aws-amplify";
 import awsExports from "@/aws-exports";
 import Head from "next/head";
 
 import "@aws-amplify/ui-react/styles.css";
-import Navbar from '@/components/Navbar/Navbar';
-import Footer from '@/components/Footer/Footer';
+import Navbar from "@/components/Navbar/Navbar";
+import Footer from "@/components/Footer/Footer";
 import "tw-elements/dist/css/tw-elements.min.css";
 
 Amplify.configure({ ...awsExports });
@@ -15,13 +15,20 @@ export default function App({ Component, pageProps }) {
     <>
       <Head>
         <title>SVG-Natacion</title>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="description"
+          content="svgnatacion, Sergio valiente Gómez Capacitacion de cursos para entrenadores y blog de natacion"
+          key="desc"
+        />
+        <meta name="robots" content="all" />
       </Head>
       <Navbar />
       <div className="py-24 px-16 bg-indigo-100">
         <Component {...pageProps} />
       </div>
       <Footer />
-
     </>
   );
 }
