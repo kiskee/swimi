42a6b168e9771c68f92076d7e938ee9b814a8a9a
Daniel Medina Cossio <daniel.medina@talent.com>
2023-10-21 17:10:51 -0500

    last changes to works

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -15,7 +15,7 @@ export default function App({ Component, pageProps }) {
     <>
       <Head>
         <title>SVG-Natacion</title>
-        <meta charset="utf-8" />
+        <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta
           name="description"
@@ -28,7 +28,7 @@ export default function App({ Component, pageProps }) {
       <div className="py-24 px-16 bg-indigo-100">
         <Component {...pageProps} />
       </div>
-      <Footer />
+      <Footer className="z-40"/>
     </>
   );
 }
